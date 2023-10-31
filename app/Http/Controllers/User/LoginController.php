0a6c69cf6ef68a45f2d8fc65e72298a5c267134a
Muhammad Dzulfiqar Rafli Anwar <rafli.anwar2001@gmail.com>
2023-10-31 15:40:11 +0700

    Login Regis Controller

--- app/Http/Controllers/User/LoginController.php
+++ app/Http/Controllers/User/LoginController.php
@@ -4,10 +4,48 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         return view('mahasiswa.login.index');
+    }
+
+    public function authenticate(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $credentials = $request->only('email', 'password');
+        // $credentials['roles'] = '1';
+
+        if (Auth::attempt($credentials)) {
+            if (auth()->user()->role_id == 1) {
+                $request->session()->regenerate();
+                return redirect()->route('admin.dashboard');
+            } elseif (auth()->user()->role_id == 2) {
+                $request->session()->regenerate();
+                return redirect()->route('user.dashboard');
+            }
+        }
+
+        return back()->withErrors([
+            'error' => 'Login failed'
+        ])->withInput();
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect()->route('user.login');
     }
 }
---
+++ app/Http/Controllers/User/RegisterController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Models\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class RegisterController extends Controller
+{
+    public function index()
+    {
+        return view('mahasiswa.auth.register');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'phone' => 'required|max:15|regex:/^[1-9][0-9]*$/',
+            'email' => 'required|email',
+            'password' => 'required|min:6'
+        ]);
+
+        $data = $request->except('_token');
+
+        $isEmailExist = User::where('email', $request->email)->exists();
+
+        if ($isEmailExist) {
+            return back()->withErrors([
+                'email' => 'Email ini sudah digunakan!'
+            ])->withInput();
+        }
+
+        $phoneNumber = $request->input('countryCode') . $request->input('phone');
+
+        $data['phone'] = $phoneNumber;
+        $data['password'] = Hash::make($request->password);
+
+        User::create($data);
+
+        return redirect()->route('user.home');
+    }
+}
