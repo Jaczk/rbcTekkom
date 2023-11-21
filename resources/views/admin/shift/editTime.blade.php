9943d9d154b25f8a11188642981289a6874b63d1
Muhammad Dzulfiqar Rafli Anwar <rafli.anwar2001@gmail.com>
2023-11-21 21:00:11 +0700

    add Shift CRUD

---
+++ resources/views/admin/shift/editTime.blade.php
@@ -0,0 +1,104 @@
+@extends('admin.layouts.base')
+
+@section('title', 'Shift')
+
+@section('content')
+    <div class="row">
+        <div class="col-md-12">
+
+            {{-- Alert Here --}}
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+
+            @if (session()->has('success'))
+                <div class="alert alert-success alert-dismissible fade show" role="alert">
+                    {{ session('success') }}
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+            @endif
+
+            <div class="card card-primary">
+                <div class="card-header" style="background-color: #6998AB">
+                    <h3 class="card-title">Perbarui Jam Layanan</h3>
+                </div>
+                <!-- /.card-header -->
+                <!-- form start -->
+                <form enctype="multipart/form-data" method="POST" action="{{ route('admin.shift.updateTime') }}">
+                    @csrf
+                    @method('PUT')
+                    <div class="card-body">
+                      <div class="form-row">
+                        <div class="col">
+                            <div class="form-group">
+                                <label for="title">Hari</label>
+                                <input type="text" class="form-control" id="day" name="day"
+                                    placeholder="Senin" value="{{ $shift->day }}">
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="form-group">
+                                <label for="title">Jam Layanan Shift 1</label>
+                                <input type="text" class="form-control" id="s1" name="s1"
+                                    placeholder="Jam Layanan Pustakawan Shift 1" value="{{ $shift->s1 }}">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-row">
+                        <div class="col">
+                            <div class="form-group">
+                                <label for="title">Jam Layanan Shift 2</label>
+                                <input type="text" class="form-control" id="s2" name="s2"
+                                    placeholder="Jam Layanan Shift 2" value="{{ $shift->s2 }}">
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="form-group">
+                                <label for="title">Jam Layanan Shift 3</label>
+                                <input type="text" class="form-control" id="s3" name="s3"
+                                    placeholder="Jam Layanan Shift 3" value="{{ $shift->s3 }}">
+                            </div>
+                        </div>
+                    </div>
+                    </div>
+                    <!-- /.card-body -->
+                    <div class="card-footer">
+                        <button type="submit" class="btn btn-primary" onclick="confirmEditForm(event)">Simpan</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@section('js')
+    <script>
+        function confirmEditForm(event) {
+            event.preventDefault(); // Prevent default form submission
+
+            Swal.fire({
+                title: 'Simpan perubahan?',
+                text: 'Jam Layanan akan diperbarui.',
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Simpan',
+                cancelButtonText: 'Kembali',
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    // User confirmed, submit the form
+                    event.target.form.submit();
+                }
+            });
+        }
+    </script>
+@endsection
---
+++ resources/views/admin/shift/index.blade.php
@@ -0,0 +1,93 @@
+@extends('admin.layouts.base')
+
+@section('title', 'Shift')
+
+@section('content')
+
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card card-primary">
+                <div class="card-header" style="background-color: #6998AB">
+                    <h3 class="card-title">Jam Layanan</h3>
+                </div>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="mx-2 mb-3">
+                            <a href="{{ route('admin.shift.editTime') }}" class="btn btn-primary text-bold">Edit Jam Layanan</a>
+                        </div>
+                    </div>
+                    @if (session()->has('success'))
+                        <div class="alert alert-success alert-dismissible fade show" role="alert">
+                            {{ session('success') }}
+                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                    @endif
+                    @if (session()->has('error'))
+                        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                            {{ session('error') }}
+                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                    @endif
+
+                    <div class="row">
+                        <div class="col-md-12">
+                            <table id="user" class="table table-striped table-hover">
+                                <thead>
+                                    <tr>
+                                        <th>Hari\Jam</th>
+                                        <th>{{ $shiftFirst->s1 }}</th>
+                                        <th>{{ $shiftFirst->s2 }}</th>
+                                        <th>{{ $shiftFirst->s3 }}</th>
+                                        <th>Aksi</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+
+                                    @foreach ($shifts as $shift)
+                                        <tr>
+                                            <td>{{ $shift->day }}</td>
+                                            <td>{{ $shift->s1 }}</td>
+                                            <td>{{ $shift->s2 }}</td>
+                                            <td>{{ $shift->s3 }}</td>
+                                            <td class="flex-row d-flex">
+                                                <a href="{{ route('admin.shift.edit', Crypt::encryptString($shift->id)) }}"
+                                                    class="btn btn-secondary">
+                                                    <i class="fas fa-edit"></i>
+                                                </a>
+                                                <!-- Button trigger modal -->
+                                                {{-- <form method="post" 
+                                                    action="{{ route('admin.shift.destroy', $shift->id) }}">
+                                                    @method('delete')
+                                                    @csrf
+                                                    <button type="submit" class="mx-2 btn btn-danger delete-btn">
+                                                        <i class="fas fa-trash"></i>
+                                                    </button>
+                                                </form> --}}
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@section('js')
+    <script>
+        $(document).ready(function() {
+            // Initialize DataTable
+            var table = $('#user').DataTable({
+                ordering: false // Disable ordering
+            });
+        });
+    </script>
+
+@endsection
