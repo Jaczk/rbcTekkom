6d6d53824d2e3f3807ba9678fd45c5004fcf93a6
Chrisz <test4hack4l1@gmail.com>
2023-11-21 21:17:09 +0700

    fix book label

--- resources/views/admin/pdf/book_labels.blade.php
+++ resources/views/admin/pdf/book_labels.blade.php
@@ -5,31 +5,79 @@
     <meta charset="utf-8">
     <title>Label Buku</title>
     <style>
+        body {
+            margin: 0;
+            padding: 0;
+        }
+
         .label {
-            width: 250px;
-            /* Adjust the width as needed */
-            height: 155px;
-            /* Adjust the height as needed */
+            width: 450px;
+            height: 100px;
             border: 1px solid #000;
-            /* Border style */
+            display: table;
+            margin: 30px;
             text-align: center;
-            /* Center-align the text */
-            padding: 10px;
+            padding: 0;
+            font-size: 14px;
+        }
+
+        .cell {
+            display: table-cell;
+            padding: 0;
+            vertical-align: middle;
+        }
+
+        .left {
+            width: 35%;
+            border-right: 1px solid #000
+                /* Add a semicolon here */
+                /* Set the left section width */
+        }
+
+
+        .right {
+            width: 65%;
+            /* Set the right section width */
+        }
+
+        .border-title {
+            border-bottom: 1px solid #000;
+        }
+
+        .d-left {
+            width: 65%;
+        }
+
+        .d-right {
+            width: 35%;
+        }
+
+        p {
+            line-height: 6px;
+
+        }
+
+        img {
+            max-width: 100%;
+            height: auto;
         }
     </style>
 </head>
 
 <body>
-    <div class="label" style="font-size: 14px;">
-        <div class="row">
-            <img src="{{ public_path('assets/images/LOGO-UNDIP-1.png') }}" alt="" class="img-responsive" width="100px">
-            <p>Ruang Baca Departemen Teknik Komputer</p>
+     <div class="label">
+            <div class="cell left">
+                <img src="{{ public_path('storage/qr-images/' . $book->qr_code) }}" alt="QR Code" width="120px">
+            </div>
+            <div class="cell right">
+                <div class="border-title">
+                    <p>Ruang Baca Departemen Teknik Komputer</p>
+                    <p>Universitas Diponegoro</p>
+                </div>
+                <p>{{ $book->lib_book_code }}</p>
+                <p>{{ strtoupper(substr($book->author, 0, 3)) }}</p>
+                <p>{{ strtolower(substr($book->book_name, 0, 1)) }}</p>
+            </div>
         </div>
-        <p>Universitas Diponegoro</p>
-        <p>{{ $book->lib_book_code }}</p>
-        <p>{{ strtoupper(substr($book->author, 0, 3)) }}</p>
-        <p>{{ strtolower(substr($book->book_name, 0, 1)) }}</p>
-    </div>
-    
 </body>
 </html>
--- resources/views/admin/pdf/multi_book_labels.blade.php
+++ resources/views/admin/pdf/multi_book_labels.blade.php
@@ -1,39 +1,88 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="utf-8">
     <title>Label Buku</title>
     <style>
+        body {
+            margin: 0;
+            padding: 0;
+        }
+
         .label {
-            width: 250px; /* Adjust the width as needed */
-            height: 155px; /* Adjust the height as needed */
-            border: 1px solid #000; /* Border style */
-            display: inline-block; /* Display cards horizontally */
-            margin: 10px; /* Adjust margin between cards */
-            text-align: center; /* Center-align the text */
-            padding: 10px;
+            width: 450px;
+            height: 100px;
+            border: 1px solid #000;
+            display: table;
+            margin: 30px;
+            text-align: center;
+            padding: 0;
+            font-size: 14px;
+        }
+
+        .cell {
+            display: table-cell;
+            padding: 0;
+            vertical-align: middle;
+        }
+
+        .left {
+            width: 35%;
+            border-right: 1px solid #000
+                /* Add a semicolon here */
+                /* Set the left section width */
+        }
+
+
+        .right {
+            width: 65%;
+            /* Set the right section width */
+        }
+
+        .border-title {
+            border-bottom: 1px solid #000;
+        }
+
+        .d-left {
+            width: 65%;
+        }
+
+        .d-right {
+            width: 35%;
+        }
+
+        p {
+            line-height: 6px;
+
+        }
+
+        img {
+            max-width: 100%;
+            height: auto;
         }
     </style>
 </head>
+
 <body>
     @foreach ($books as $book)
-    <div class="label" style="font-size: 14px;">
-        <div class="row">
-            <img src="{{ public_path('assets/images/LOGO-UNDIP-1.png') }}" alt="" class="img-responsive" width="100px">
-            <p>Ruang Baca Departemen Teknik Komputer</p>
+        <div class="label">
+            <div class="cell left">
+                <img src="{{ public_path('storage/qr-images/' . $book->qr_code) }}" alt="QR Code" width="120px">
+            </div>
+            <div class="cell right">
+                <div class="border-title">
+                    <p>Ruang Baca Departemen Teknik Komputer</p>
+                    <p>Universitas Diponegoro</p>
+                </div>
+
+                <p>{{ $book->lib_book_code }}</p>
+                <p>{{ strtoupper(substr($book->author, 0, 3)) }}</p>
+                <p>{{ strtolower(substr($book->book_name, 0, 1)) }}</p>
+
+            </div>
         </div>
-        <p>Universitas Diponegoro</p>
-        <p>{{ $book->lib_book_code }}</p>
-        <p>{{ strtoupper(substr($book->author, 0, 3)) }}</p>
-        <p>{{ strtolower(substr($book->book_name, 0, 1)) }}</p>
-    </div>
     @endforeach
 </body>
+
 </html>
-<script type="text/javascript">
-    // Wait for the document to fully load
-    window.addEventListener('load', function () {
-        // Trigger the print dialog
-        window.print();
-    });
-</script>
